dcc3b01deb3183d85b6db8d7ac7fac7ec569d7a1
Sandeep Kumar <sandeepkumar073113@gmail.com>
2025-03-17 10:07:22 +0530

    [Sandeep ADD : Adding Book details functionality

---
+++ src/components/Same/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='w-full bg-[#2E1D1E] h-[50px]'>
+            <div className='max-w-6xl p-4 xl:p-0 h-full flex text-[.6rem] md:text-xs items-center mx-auto  text-white'>
+                <p>Copyright © 2020, Bookstore Private Limited. All Rights Reserved</p>
+            </div>
+        </div>
+    )
+
+}
+
+export default Footer
